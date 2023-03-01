63a8d7a260946cbb7df6dc5dacf4a2ae15cfed44
Nodir Khodjaev <nodirkhujaev@gmail.com>
2023-03-01 21:04:31 +0900

    minor fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import styles from '../styles/Navbar.module.css';
-
+import React from "react";
+import styles from "../styles/Navbar.module.css";
 
 export const Navbar = () => {
-    return (
+  return (
     <React.Fragment>
+      <header className={styles.navbar_module}>
         <div className={styles.navbar_container}>
-            <h1 className={styles.navbar_title}>Movie Recommender</h1>
-            <nav className={styles.navbar}>
-                <ul className={styles.navbar_items}>
-                    <li className={styles.navbar_item}><a href='google.com'>Search by interests</a></li>
-                    <li className={styles.navbar_item}><a href='google.com'>Search by genre</a></li>
-                </ul>
-            </nav>
+          <h1 className={styles.navbar_title}>Movie Recommender</h1>
+          <nav className={styles.navbar}>
+            <ul className={styles.navbar_items}>
+              <li className={styles.navbar_item}>
+                <a href="google.com">Search by interests</a>
+              </li>
+              <li className={styles.navbar_item}>
+                <a href="google.com">Search by genre</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-    </React.Fragment>)
-}
+      </header>
+    </React.Fragment>
+  );
+};
 
 export default Navbar;
