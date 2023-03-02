c307d8d1de7512dd0a9d2c055a932f6ff5e81c1b
Nodir Khodjaev <nodirkhujaev@gmail.com>
2023-03-02 12:47:09 +0900

    fix Dockerfile and run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-git clone https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
-
-cd ./Software-Engineering---CSE36401/Milestone4/cse364-project || exit
+git clone https://github.com/nodirkh/movie-recommender.git
+cd ./movie-recommender/
 
 mongod --fork --syslog --dbpath src/main/resources
 mvn clean spring-boot:run
