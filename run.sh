f4dc559ac452efb33efac0f818c5f0ec5777cca9
changhwa <nodirkhujaev@gmail.com>
2023-03-16 20:32:37 +0900

    WiP run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/AzmtMrzbkv/Movie-Recommendation-System.git
-cd ./Movie-Recommendation-System/
+cd ./Movie-Recommendation-System
 
 mongod --fork --syslog --dbpath src/main/resources
 mvn clean spring-boot:run
